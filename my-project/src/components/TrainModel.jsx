a7f3a7415e6d640f388b1bec57f56d1f01d82ce4
yiru jang <z5480998@ad.unsw.edu.au>
2025-09-11 23:53:12 +1000

    Loaded train model for testing

--- my-project/src/components/TrainModel.jsx
+++ my-project/src/components/TrainModel.jsx
@@ -1,8 +1,25 @@
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useTexture } from "@react-three/drei";
+import * as THREE from "three";
 
 export default function TrainModel(props) {
     // this hook loads the .glb file
     const { scene } = useGLTF("/models/train.glb"); // put train.glb in /public/models
+
+    const texture = useTexture("/textures/train_txt.webp");
+    texture.flipY = false;
+    texture.colorSpace = THREE.SRGBColorSpace; // ensures correct colors
+
+    scene.traverse((obj) => {
+        if (obj.isMesh) {
+            const geometry = obj.geometry;
+            geometry.setAttribute('uv', geometry.attributes.uv1);
+            obj.geometry.computeVertexNormals();
+        obj.material = new THREE.MeshBasicMaterial({
+            map: texture,
+            toneMapped: false, // avoids washed-out colors
+        });
+        }
+    });
 
     return <primitive object={scene} {...props} />;
 }
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -5,16 +5,17 @@ import TrainModel from "../components/TrainModel";
 export default function Home() {
     return (
         <Canvas 
-            camera={{ position: [0, 3, 3], fov: 50 }}>
+            style={{ width: '100%', height: '100vh' }}
+            camera={{ position: [0, 0, 40], fov: 50 }}>
             {/* lights */}
             <ambientLight intensity={0.5} />
             <directionalLight position={[5, 5, 5]} intensity={1} />
 
             {/* your Blender model */}
-            <TrainModel scale={0.5} position={[0, 0, 0]} />
+            <TrainModel scale={1} position={[0, 0, 0]} />
 
             {/* controls */}
-            <OrbitControls target={[0, 3, 0]}/>
+            <OrbitControls target={[0, 0, 0]}/>
         </Canvas>
     );
 }
